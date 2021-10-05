e287d5d6c0889eabeb59903dbfb7c4b58a6190de
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2021-10-05 12:00:40 -0300

    Adicionando produtos ao carrinho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
-
 import { ProductList } from "./styles";
 import { api } from "../../services/api";
 import { formatPrice } from "../../util/format";
 import { useCart } from "../../hooks/useCart";
-
 interface Product {
   id: number;
   title: string;
   price: number;
   image: string;
 }
-
 interface ProductFormatted extends Product {
   priceFormatted: string;
 }
-
 interface CartItemsAmount {
   [key: number]: number;
 }
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-
   const { addProduct, cart } = useCart();
 
-  /*   const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    // TODO
-  }, {} as CartItemsAmount); */
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = { ...sumAmount };
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
@@ -39,14 +37,14 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
     <ProductList>
       {products.map((product) => (
         <li key={product.id}>
-          <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+          <img src={product.image} alt={product.title} />
           <strong>{product.title}</strong>
           <span>{formatPrice(product.price)}</span>
           <button
@@ -56,7 +54,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */}
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
